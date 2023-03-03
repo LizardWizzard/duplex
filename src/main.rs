e9f9ab6975062c9b9ffab35d8c3bc986cae6b63d
Dmitry Rodionov <captfrozenberg@gmail.com>
2023-03-03 15:26:58 +0200

    simple example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
+use bytes::BytesMut;
 use tokio::{
-    io,
-    net::TcpListener,
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
     sync::mpsc::{Receiver, Sender},
 };
+use tokio_util::codec::{Decoder, Encoder};
 
 const MAX: usize = 8 * 1024 * 1024;
+const ADDR: &str = "127.0.0.1:34254";
 
 mod decoder;
 mod duplex;
 mod encoder;
 
 async fn echo_worker(mut receiver: Receiver<String>, sender: Sender<String>) {
+    println!("echo: waiting");
     while let Some(msg) = receiver.recv().await {
+        println!("echo: got message {}", msg);
         sender.send(msg).await.expect("disconnected");
+        println!("echo: sent");
     }
 }
 
 async fn server() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:34254").await?;
+    let listener = TcpListener::bind(ADDR).await?;
 
     let (stream, _) = listener.accept().await?;
 
@@ -37,7 +43,40 @@ async fn server() -> io::Result<()> {
 }
 
 async fn client() -> io::Result<()> {
-    todo!()
+    let mut stream = TcpStream::connect(ADDR).await?;
+
+    let mut encoder = encoder::MyStringEncoder {};
+    let mut decoder = decoder::MyStringDecoder {};
+    let mut buf = BytesMut::new();
+
+    // ping pong
+    for i in 0..2 {
+        let msg = format!("Hello {}", i);
+        println!("sending: {}", msg);
+        encoder
+            .encode(format!("Hello {}", i), &mut buf)
+            .expect("encode failed");
+        stream.write_all_buf(&mut buf).await?;
+        buf.clear();
+        loop {
+            // The read_buf call will append to buf rather than overwrite existing data.
+            let len = stream.read_buf(&mut buf).await?;
+
+            if len == 0 {
+                while let Some(frame) = decoder.decode_eof(&mut buf)? {
+                    println!("received: {}", frame);
+                }
+                break;
+            }
+
+            while let Some(frame) = decoder.decode(&mut buf)? {
+                println!("received: {}", frame);
+            }
+        }
+        buf.clear()
+    }
+
+    Ok(())
 }
 
 #[tokio::main]
