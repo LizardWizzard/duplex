ae8356505755e3093df2e2e7804052873ff1e36c
Dmitry Rodionov <captfrozenberg@gmail.com>
2023-03-03 14:47:37 +0200

    SenderCtx/ReceiverCtx

--- src/duplex.rs
+++ src/duplex.rs
@@ -54,12 +54,46 @@ fn try_recv<T>(receiver: &mut Receiver<T>) -> Result<Option<T>, ()> {
     }
 }
 
+struct SenderCtx<F, D: Decoder<Item = F>> {
+    sender: Sender<F>,
+    decoder: D,
+    decode_buf: BytesMut,
+}
+
+impl<F, D: Decoder<Item = F>> SenderCtx<F, D> {
+    fn new(sender: Sender<F>, decoder: D) -> Self {
+        Self {
+            sender,
+            decoder,
+            decode_buf: BytesMut::new(),
+        }
+    }
+}
+
+struct ReceiverCtx<F, E: Encoder<F>> {
+    receiver: Receiver<F>,
+    encoder: E,
+    // encode buf contains only one message we've received from processor
+    // if we want to store more messages we need to take care of fairness
+    encode_buf: BytesMut,
+}
+
+impl<F, E: Encoder<F>> ReceiverCtx<F, E> {
+    fn new(receiver: Receiver<F>, encoder: E) -> Self {
+        Self {
+            receiver,
+            encoder,
+            encode_buf: BytesMut::new(),
+        }
+    }
+}
+
 pub async fn spawn_duplex<F, E, D>(
     stream: TcpStream,
-    mut decoder: D,
-    mut encoder: E,
+    decoder: D,
+    encoder: E,
     sender: Sender<F>,
-    mut receiver: Receiver<F>,
+    receiver: Receiver<F>,
 ) -> Result<(), Error<D::Error, E::Error>>
 where
     E: Encoder<F>,
@@ -67,10 +101,8 @@ where
     D: Decoder<Item = F>,
     D::Error: std::error::Error,
 {
-    // encode buf contains only one message we've received from processor
-    // if we want to store more messages we need to take care of fairness
-    let mut encode_buf = BytesMut::new();
-    let mut decode_buf = BytesMut::new();
+    let mut sender_ctx = SenderCtx::new(sender, decoder);
+    let mut receiver_ctx = ReceiverCtx::new(receiver, encoder);
 
     // send to processor
     let mut pending_send_slot: Option<F> = None;
@@ -82,22 +114,23 @@ where
 
         match pending_send_slot.take() {
             Some(frame) => {
-                pending_send_slot =
-                    try_send(&sender, frame).map_err(|_| Error::Internal("processor is dead"))?;
+                pending_send_slot = try_send(&sender_ctx.sender, frame)
+                    .map_err(|_| Error::Internal("processor is dead"))?;
             }
             None => {
                 'inner: loop {
-                    match bail_on_non_would_block(stream.try_read_buf(&mut decode_buf))
+                    match bail_on_non_would_block(stream.try_read_buf(&mut sender_ctx.decode_buf))
                         .map_err(|e| Error::Io(e))?
                     {
                         // we read something, feed the buf to decoder
                         Some(_) => {
-                            if let Some(frame) = decoder
-                                .decode(&mut decode_buf)
+                            if let Some(frame) = sender_ctx
+                                .decoder
+                                .decode(&mut sender_ctx.decode_buf)
                                 .map_err(|e| Error::Decode(e))?
                             {
                                 // we managed to decode the frame, try to send it, if channel is full store frame in slot
-                                pending_send_slot = try_send(&sender, frame)
+                                pending_send_slot = try_send(&sender_ctx.sender, frame)
                                     .map_err(|_| Error::Internal("processor is dead"))?;
                                 break 'inner;
                             }
@@ -108,23 +141,26 @@ where
             }
         }
 
-        if encode_buf.is_empty() {
+        if receiver_ctx.encode_buf.is_empty() {
             // we dont have something to write into the socket
             // try to receive and write if we got something (without attempting to get next message to be fair)
-            match try_recv(&mut receiver).map_err(|_| Error::Internal("processor is dead"))? {
+            match try_recv(&mut receiver_ctx.receiver)
+                .map_err(|_| Error::Internal("processor is dead"))?
+            {
                 Some(frame) => {
-                    encoder
-                        .encode(frame, &mut encode_buf)
+                    receiver_ctx
+                        .encoder
+                        .encode(frame, &mut receiver_ctx.encode_buf)
                         .map_err(|e| Error::Encode(e))?;
 
                     // TODO cleanup, repetitive
                     'inner: loop {
-                        match bail_on_non_would_block(stream.try_write(&encode_buf))
+                        match bail_on_non_would_block(stream.try_write(&receiver_ctx.encode_buf))
                             .map_err(|e| Error::Io(e))?
                         {
                             Some(written) => {
-                                encode_buf.advance(written);
-                                if encode_buf.is_empty() {
+                                receiver_ctx.encode_buf.advance(written);
+                                if receiver_ctx.encode_buf.is_empty() {
                                     // do not attempt to receive new message, go to other duties to be fair
                                     break 'inner;
                                 }
@@ -142,18 +178,19 @@ where
             // we have something to write into the socket
             // try to write it and if we wrote everything try to get new message but dont start writing it to be fair
             'inner: loop {
-                match bail_on_non_would_block(stream.try_write(&encode_buf))
+                match bail_on_non_would_block(stream.try_write(&receiver_ctx.encode_buf))
                     .map_err(|e| Error::Io(e))?
                 {
                     Some(written) => {
-                        encode_buf.advance(written);
-                        if encode_buf.is_empty() {
-                            match try_recv(&mut receiver)
+                        receiver_ctx.encode_buf.advance(written);
+                        if receiver_ctx.encode_buf.is_empty() {
+                            match try_recv(&mut receiver_ctx.receiver)
                                 .map_err(|_| Error::Internal("processor is dead"))?
                             {
                                 Some(frame) => {
-                                    encoder
-                                        .encode(frame, &mut encode_buf)
+                                    receiver_ctx
+                                        .encoder
+                                        .encode(frame, &mut receiver_ctx.encode_buf)
                                         .map_err(|e| Error::Encode(e))?;
                                 }
                                 None => {
@@ -186,7 +223,7 @@ where
         tokio::select! {
             _ = stream.ready(interest.unwrap()), if interest.is_some() => {},
             // if we have send slot we must've blocked on try_send so we can wait on sender.reserve
-            _ = sender.reserve(), if pending_send_slot.is_some() => {},
+            _ = sender_ctx.sender.reserve(), if pending_send_slot.is_some() => {},
             // TODO if we blocked on recv we can wait on receiver.peek()
             _ = tokio::task::yield_now(), if receive_blocked => {}
         }
