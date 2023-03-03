726296457a7625e2fd26943b3d77f2fe6305a354
Dmitry Rodionov <captfrozenberg@gmail.com>
2023-03-03 19:31:33 +0200

    debug

--- src/duplex.rs
+++ src/duplex.rs
@@ -14,18 +14,19 @@ use tokio_util::codec::Decoder;
 use tokio_util::codec::Encoder;
 
 #[derive(thiserror::Error, Debug)]
-pub enum Error<D: std::error::Error, E: std::error::Error> {
+pub enum Error<D, E> {
     Decode(D),
     Encode(E),
     Io(std::io::Error),
     Internal(&'static str),
 }
 
-fn bail_on_non_would_block(r: io::Result<usize>) -> io::Result<Option<usize>> {
+fn bail_on_non_would_block(r: io::Result<usize>, ctx: &'static str) -> io::Result<Option<usize>> {
     match r {
         Ok(v) => Ok(Some(v)),
         Err(e) => {
             if e.kind() == ErrorKind::WouldBlock {
+                println!("would block {}", ctx);
                 return Ok(None);
             } else {
                 return Err(e);
@@ -119,8 +120,11 @@ where
             }
             None => {
                 'inner: loop {
-                    match bail_on_non_would_block(stream.try_read_buf(&mut sender_ctx.decode_buf))
-                        .map_err(|e| Error::Io(e))?
+                    match bail_on_non_would_block(
+                        stream.try_read_buf(&mut sender_ctx.decode_buf),
+                        "try_read",
+                    )
+                    .map_err(|e| Error::Io(e))?
                     {
                         // we read something, feed the buf to decoder
                         Some(_) => {
@@ -135,7 +139,10 @@ where
                                 break 'inner;
                             }
                         }
-                        None => read_blocked = true,
+                        None => {
+                            read_blocked = true;
+                            break 'inner;
+                        }
                     }
                 }
             }
@@ -155,8 +162,11 @@ where
 
                     // TODO cleanup, repetitive
                     'inner: loop {
-                        match bail_on_non_would_block(stream.try_write(&receiver_ctx.encode_buf))
-                            .map_err(|e| Error::Io(e))?
+                        match bail_on_non_would_block(
+                            stream.try_write(&receiver_ctx.encode_buf),
+                            "try_write_1",
+                        )
+                        .map_err(|e| Error::Io(e))?
                         {
                             Some(written) => {
                                 receiver_ctx.encode_buf.advance(written);
@@ -178,8 +188,11 @@ where
             // we have something to write into the socket
             // try to write it and if we wrote everything try to get new message but dont start writing it to be fair
             'inner: loop {
-                match bail_on_non_would_block(stream.try_write(&receiver_ctx.encode_buf))
-                    .map_err(|e| Error::Io(e))?
+                match bail_on_non_would_block(
+                    stream.try_write(&receiver_ctx.encode_buf),
+                    "try_write_2",
+                )
+                .map_err(|e| Error::Io(e))?
                 {
                     Some(written) => {
                         receiver_ctx.encode_buf.advance(written);
@@ -211,6 +224,7 @@ where
         // what we need to wait for in order to make progress?
         // if we would blocked on read, we wait for Interest::READABLE
         // if we would blocked on write, we wait for Interest::WRITABLE
+        dbg!(read_blocked, write_blocked);
         let interest = {
             match (read_blocked, write_blocked) {
                 (false, false) => None,
@@ -221,12 +235,17 @@ where
         };
 
         tokio::select! {
-            _ = stream.ready(interest.unwrap()), if interest.is_some() => {},
+            _ = stream.ready(interest.unwrap()), if interest.is_some() => {
+                println!("stream ready")
+            },
             // if we have send slot we must've blocked on try_send so we can wait on sender.reserve
-            _ = sender_ctx.sender.reserve(), if pending_send_slot.is_some() => {},
+            _ = sender_ctx.sender.reserve(), if pending_send_slot.is_some() => {
+                println!("sender ready")
+            },
             // TODO if we blocked on recv we can wait on receiver.peek()
             _ = tokio::task::yield_now(), if receive_blocked => {}
         }
     }
     // no shutdown other than with Error
 }
+
